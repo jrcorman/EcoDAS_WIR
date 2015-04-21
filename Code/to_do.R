b37429d97f30020a5e6b84ebc013a30c2b54d583
jrcorman <jrcorman@asu.edu>
2015-04-21 12:11:35 -0500

    Added to do list.

---
+++ Code/to_do.R
@@ -0,0 +1,13 @@
+### Things to graph
+### Based on 21 April meeting
+
+# What is different between man-made and natural lakes?
+
+# secchi disk
+
+# temperature (more difficult to interpret, tho)
+
+# Catchment area : lake area
+
+# scatter plot of secchi disk by catchment area : lake area 
+# with distintion between man-made and natural lake
