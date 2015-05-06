396bfd18b5a76000a31669685d427f93a518b38b
brdeemer <bridget.deemer@email.wsu.edu>
2015-05-06 13:14:57 -0700

    Added code for secchi disk depth, perimeter, catchment area:lake area, and surface area.

--- Code/to_do.R
+++ Code/to_do.R
@@ -3,11 +3,11 @@
 
 # What is different between man-made and natural lakes?
 
-# secchi disk
+# secchi disk- boxplots done BD
 
 # temperature (more difficult to interpret, tho)
 
-# Catchment area : lake area
+# Catchment area : lake area- boxplots done BD
 
 # scatter plot of secchi disk by catchment area : lake area 
-# with distintion between man-made and natural lake
+# with distintion between man-made and natural lake- done BD
