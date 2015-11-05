4bbfaabaa51b0bf08fd5e8adde9dd8650f85d597
jrcorman <jrcorman@asu.edu>
2015-11-05 12:59:05 -0600

    Updated to make black and white figures

--- Code/lake_pair_data_alignment.R
+++ Code/lake_pair_data_alignment.R
@@ -54,7 +54,7 @@ pair <- function(filename, analyte){
   cdata$avg_all <- cdata$avg
   return(cdata)
 }
-# Modification for temperature plots, a sets Depth
+# Modification for temperature plots, a set Depth
 pair.temp <- function(filename, analyte, a){
   data <- read.csv(filename)
   res.all <- subset(data, SITE_ID %in% res.names)
@@ -84,12 +84,14 @@ dgraph <- function(df, xtitle, legend){
                  mean = mean(avg_all, na.rm=TRUE),
                  median = median(avg_all, na.rm=TRUE))
   p <- ggplot(df, aes(x=avg, fill=type)) + 
-    geom_density(alpha=.3) +
+    geom_density(alpha=.75, size=1, aes(colour=type)) +
     geom_vline(data=sdata, aes(xintercept=mean,  colour=type),
                linetype="solid", size=1) +
     geom_vline(data=sdata, aes(xintercept=median,  colour=type),
                linetype="dashed", size=1) +
     theme_classic(14) +
+    scale_fill_manual(values=c("#222222", "white")) +
+    scale_color_manual(values=c("#222222", "#999999")) +
     scale_y_continuous(expand=c(0,0)) +
     scale_x_continuous(expand=c(0,0)) +
     xlab(xtitle)
@@ -102,7 +104,7 @@ dgraph <- function(df, xtitle, legend){
 sec <- pair("Data/NLA2007_secchi.csv", "SECMEAN")
 sec$avg[sec$avg > quantile(sec$avg, prob=0.99, na.rm=TRUE)] <- quantile(sec$avg, prob=0.99, na.rm=TRUE)
 dsec <- dgraph(sec, "Secchi Depth (m)", "no")
-
+dsec
 # Temp at surface
 temp_1m <- pair.temp("Data/NLA2007_temp.csv", "TEMP_FIELD", 1)
 dtemp_1m <- dgraph(temp_1m, "Temp (oC) Surface", "no")
@@ -127,17 +129,45 @@ names(tdatab) <- c("SITE_ID", "PAIR_ID", "avg", "type")
 tdatab$avg_all <- tdatab$avg
 dtemp_bottom <- dgraph(tdatab, "Bottom Temp (oC)", "no")
 
+# Stratification proxy (Difference between top and bottom/depth)
+temp <- read.csv("Data/NLA2007_temp.csv") #temp data
+res.temp<- subset(temp, SITE_ID %in% res.names)
+lakes.temp <- subset(temp, SITE_ID %in% lakes.names)
+# there are multiple records of temp for some sites
+# so, taking average of multiple readings
+res.tempmean.s <- ddply(res.temp, .(SITE_ID), summarize,
+                          templow = min(TEMP_FIELD),
+                          temphigh = max(TEMP_FIELD),
+                          depth.m = max(DEPTH))
+res.tempmean.s$type <- rep("reservoir")
+lakes.tempmean.s <- ddply(lakes.temp, .(SITE_ID), summarize,
+                            templow = min(TEMP_FIELD),
+                            temphigh = max(TEMP_FIELD),
+                            depth.m = max(DEPTH))
+lakes.tempmean.s$type <- rep("lakes")
+strat <- rbind(res.tempmean.s, lakes.tempmean.s)
+strat$delTemp_Depth <- (strat$temphigh - strat$templow)/strat$depth.m
+pstrat <- merge(pairs, strat, by = "SITE_ID")
+names(pstrat) <- c("SITE_ID", "PAIR_ID", "tl", "th", "depth.m", "type", "avg")
+pstrat$avg_all <- pstrat$avg
+dtemp_change <- dgraph(pstrat, "del Temp m-1", "no")
+
 # Catchment Area
 CA_all <- pair("Data/NLA2007_basin.csv", "BASINAREA_KM2")
+pr <- 0.92
+CA_all$avg[CA_all$avg > quantile(CA_all$avg, prob=pr, na.rm=TRUE)] <- quantile(CA_all$avg, prob=pr, na.rm=TRUE)
 dCA <- dgraph(CA_all, "Catchment Area (km2)", "no")
 dCA
 
-# Catchment area : lake area
+# Lake area
 LA <- pair("Data/NLA2007_lakes.csv", "LAKEAREA") #in km2
 LAs <- LA
 pr <- 0.9
 LAs$avg[LAs$avg > quantile(LAs$avg, prob=pr, na.rm=TRUE)] <- quantile(LAs$avg, prob=pr, na.rm=TRUE)
 dLA <- dgraph(LAs, "Lake Area (km2)", "no")
+
+# Catchment area : lake area
+CA <- pair("Data/NLA2007_basin.csv", "BASINAREA_KM2")
 names(CA) <- c("Site", "PAIR_ID", "Basin.km2", "type")
 names(LA) <- c("Site", "PAIR_ID", "Lake.km2", "type")
 CA$id <- paste(CA$Site, "_", CA$type)
@@ -146,7 +176,7 @@ CA_LA <- merge(CA, LA, by = "id")
 CA_LA$avg <- CA_LA$Basin.km2/CA_LA$Lake.km2
 CA_LA$avg_all <- CA_LA$avg
 CA_LA$type <- CA_LA$type.y
-pr2 <- 0.95
+pr2 <- 0.9
 CA_LA$avg[CA_LA$avg > quantile(CA_LA$avg, prob=pr2, na.rm=TRUE)] <- quantile(CA_LA$avg, prob=pr2, na.rm=TRUE)
 dCALA <- dgraph(CA_LA, "CA:LA", "no")
 dCALA
@@ -155,23 +185,24 @@ dCALA
 peri <- pair("Data/NLA2007_lakes.csv", "LAKEPERIM")
 pr3 <- 0.95
 peri$avg[peri$avg > quantile(peri$avg, prob=pr3, na.rm=TRUE)] <- quantile(peri$avg, prob=pr3, na.rm=TRUE)
-dperi <- dgraph(peri, "Perimeter (units)", "no")
+dperi <- dgraph(peri, "Perimeter (m)", "no")
 dperi
 
 # Elevation
 el <- pair("Data/NLA2007_lakes.csv", "ELEV_PT")
-del <- dgraph(el, "Elevation (units)", "yes")
+del <- dgraph(el, "Elevation (m)", "yes")
 del
 
 # Max Depth
 depth <- pair("Data/NLA2007_lakes.csv", "DEPTHMAX")
-ddepth <- dgraph(depth, "Depth (units)", "no")
+ddepth <- dgraph(depth, "Max Depth (m)", "no")
 str(depth)
 boxplot(depth$avg ~ depth$type)
 
 # All density plots as one
 library(gridExtra)
-grid.arrange(del, dCALA, dLA, dperi, ddepth, dsec, dtemp_1m, dtemp_bottom, ncol=2)
+grid.arrange(del, dCALA, dCA, dLA, dperi, 
+             ddepth, dsec, dtemp_1m, dtemp_bottom, dtemp_change, ncol=2)
 
 # All boxplots as one
 par(mfrow=c(2,2))
